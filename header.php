28185d468cf72894dd7e4a22c32369ef02350ff4
lorussian <silviolorusso@gmail.com>
2013-12-25 18:45:00 +0100

    beta label

--- header.php
+++ header.php
@@ -52,6 +52,7 @@
 	</head>
 
 	<body <?php body_class(); ?>>
+		<h5 id="beta" style="position:absolute; top: 0; right:0; margin-top:0.2em; margin-right:0.3em; color:red;">BETA!</h5>
 		<div id="container">
 			<header class="header" role="banner">
 				<div id="inner-header" class="wrap clearfix">
