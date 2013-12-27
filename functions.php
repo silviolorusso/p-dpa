db07c25f02e4c2f582d5354291952d1eab0d0a11
lorussian <silviolorusso@gmail.com>
2013-12-27 22:14:22 +0100

    cleaned functions.php

--- functions.php
+++ functions.php
@@ -20,26 +20,6 @@ URL: http://silviolorusso.com/
 	- adding custom fields to user profiles
 */
 require_once( 'library/bones.php' ); // if you remove this, bones will break
-/*
-2. library/custom-post-type.php
-	- an example custom post type
-	- example custom taxonomy (like categories)
-	- example custom taxonomy (like tags)
-*/
-// require_once( 'library/custom-post-type.php' ); // you can disable this if you like
-/*
-3. library/admin.php
-	- removing some default WordPress dashboard widgets
-	- an example custom dashboard widget
-	- adding custom login css
-	- changing text in footer of admin
-*/
-// require_once( 'library/admin.php' ); // this comes turned off by default
-/*
-4. library/translation/translation.php
-	- adding support for other languages
-*/
-// require_once( 'library/translation/translation.php' ); // this comes turned off by default
 
 /************* THUMBNAIL SIZE OPTIONS *************/
 
