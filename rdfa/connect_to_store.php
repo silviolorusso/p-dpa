3525210293ef93fda03fa9ff654c3f0df23f1c6d
lorussian <silviolorusso@gmail.com>
2013-12-29 16:55:35 +0100

    connect to store

---
+++ rdfa/connect_to_store.php
@@ -0,0 +1,34 @@
+<?php
+// ************************************************************
+// Connecting to the Store
+// ************************************************************
+
+/* MySQL and endpoint configuration */ 
+$config = array(
+  /* db */
+  'db_host' => 'localhost', /* optional, default is localhost */
+  'db_name' => 'p-dpa-sparql',
+  'db_user' => 'root',
+  'db_pwd' => 'root',
+
+  /* store name */
+  'store_name' => 'my_endpoint_store',
+
+  /* endpoint */
+  'endpoint_features' => array(
+    'select', 'construct', 'ask', 'describe', 
+    'load', 'insert', 'delete', 
+    'dump' /* dump is a special command for streaming SPOG export */
+  ),
+  'endpoint_timeout' => 60, /* not implemented in ARC2 preview */
+  'endpoint_write_key' => '********', /* optional, but without one, everyone can write! */
+  // 'endpoint_max_limit' => 300, /* optional */
+);
+
+/* instantiation */
+$store = ARC2::getStoreEndpoint($config);
+
+if (!$store->isSetUp()) {
+  $store->setUp(); /* create MySQL tables */
+}
+?>
