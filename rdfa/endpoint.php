b49277d5483548312af564c0b82552853af7e8d1
lorussian <silviolorusso@gmail.com>
2013-12-30 03:16:07 +0100

    endpoint update

--- rdfa/endpoint.php
+++ rdfa/endpoint.php
@@ -11,13 +11,6 @@
 require 'lib/arc2/ARC2.php';
 include_once('connect_to_store.php');
 
-/* instantiation */
-$store = ARC2::getStoreEndpoint($config);
-
-if (!$store->isSetUp()) {
-  $store->setUp(); /* create MySQL tables */
-}
-
 $store->go();
 ?>
 
