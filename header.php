d5864d24ea09df0760d750ee303f50de84aca950
lorussian <silviolorusso@gmail.com>
2016-03-06 10:16:09 +0100

    remover google analytics

--- header.php
+++ header.php
@@ -36,18 +36,6 @@
 
 		<?php wp_head(); ?>
 		
-		<!-- Analytics -->
-		<script>
-		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		
-		  ga('create', 'UA-17080297-56', 'p-dpa.net');
-		  ga('send', 'pageview');
-		
-		</script>
-		
 		<!-- FitVids -->
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/library/js/libs/jquery.fitvids.js"></script>
 		<!-- Masonry -->
